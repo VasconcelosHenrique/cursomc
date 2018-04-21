de61d64b21bc8ed47223db4871a5c0536de07a40
Henrique Vasconcelos <henriquenvasconcelos@gmail.com>
2018-04-21 20:43:53 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/henriquevasconcelos/cursomc/services/CategoriaService.java
+++ src/main/java/com/henriquevasconcelos/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.henriquevasconcelos.cursomc.domain.Categoria;
+import com.henriquevasconcelos.cursomc.dto.CategoriaDTO;
 import com.henriquevasconcelos.cursomc.repositories.CategoriaRepository;
 import com.henriquevasconcelos.cursomc.services.exceptions.DataIntegrityException;
 import com.henriquevasconcelos.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 				
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
