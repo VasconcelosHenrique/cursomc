8a282c6148a132b17c1564778d05e98cfb71f1d5
Henrique Vasconcelos <henriquenvasconcelos@gmail.com>
2018-04-22 11:17:56 -0300

    Ajustando a exclusão de Cliente

--- src/main/java/com/henriquevasconcelos/cursomc/services/ClienteService.java
+++ src/main/java/com/henriquevasconcelos/cursomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+			throw new DataIntegrityException("Não é possivel porque há entidades relacionadas");
 		}
 	}
 	
