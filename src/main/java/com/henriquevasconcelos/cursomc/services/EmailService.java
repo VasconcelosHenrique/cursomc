ab1104c5666853cdd3b72596ec01b240277d9f45
Henrique Vasconcelos <henriquenvasconcelos@gmail.com>
2018-05-01 11:08:11 -0300

    Email HTML

--- src/main/java/com/henriquevasconcelos/cursomc/services/EmailService.java
+++ src/main/java/com/henriquevasconcelos/cursomc/services/EmailService.java
@@ -1,12 +1,18 @@
 package com.henriquevasconcelos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.henriquevasconcelos.cursomc.domain.Pedido;
 
 public interface EmailService {
 
-	void sendOrderConfirmationEmail(Pedido obj);
+void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
